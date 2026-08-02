8dd48bb407aa2a8ea066c66f1d6085c00698b9b2
Andrew Caswell <andrew.s.caswell@gmail.com>
2026-08-02 19:59:43 +0100

    implement section 2

--- main.rs
+++ main.rs
@@ -1,4 +1,9 @@
-use std::{char, io::{self, BufRead}, iter::Peekable, str::CharIndices};
+use std::{
+    char,
+    io::{self, BufRead},
+    iter::{FromIterator, Peekable},
+    str::CharIndices,
+};
 
 // TODO (json-tokenize): implement per the lesson description.
 
@@ -6,7 +11,9 @@ fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let l = line.unwrap();
-        if l.is_empty() { continue; }
+        if l.is_empty() {
+            continue;
+        }
 
         let mut t = Tokinzer::new(&l);
         t.tokenize();
@@ -18,7 +25,7 @@ pub struct Tokinzer<'a> {
 }
 
 impl<'a> Tokinzer<'a> {
-    pub fn new(input: &'a str) -> Self{
+    pub fn new(input: &'a str) -> Self {
         Tokinzer {
             chars: input.char_indices().peekable(),
         }
@@ -36,66 +43,84 @@ impl<'a> Tokinzer<'a> {
         while let Some((_, ch)) = self.peek() {
             if ch.is_whitespace() {
                 self.next();
-            }
-            else {
+            } else {
                 break;
             }
         }
     }
 
-    pub fn tokenize(&mut self) { 
+    pub fn tokenize(&mut self) {
         while let Some((idx, ch)) = self.peek() {
             match ch {
                 '{' | '}' | '[' | ']' | ':' | ',' => {
                     println!("PUNCT {}", ch);
                     self.next();
-                },
-                '"' => { 
-                    println!("STRING {}", self.read_string());
-                },
+                }
+                '"' => {
+                    match self.read_string() {
+                        Ok(string) => { println!("{}", string); },
+                        Err(_) => { 
+                            println!("ERR");
+                            return;
+                        }
+                    }
+                    
+                }
                 ch if ch.is_ascii_digit() || ch == '-' => {
                     println!("NUMBER {}", self.read_number());
-                },
+                }
                 ch if ch.is_whitespace() => {
                     self.skip_whitespace();
-                },
+                }
                 't' | 'f' | 'n' => {
                     let value = self.read_value();
                     match value.as_str() {
-                        "true" => { println!("TRUE {}", value); },
-                        "false" => { println!("FALSE {}", value); },
-                        "null" => { println!("NULL {}", value); },
-                        _ => { println!("ERR invalid token {}", value); }
-
+                        "true" => {
+                            println!("TRUE {}", value);
+                        }
+                        "false" => {
+                            println!("FALSE {}", value);
+                        }
+                        "null" => {
+                            println!("NULL {}", value);
+                        }
+                        _ => {
+                            println!("ERR invalid token {}", value);
+                        }
                     }
-                },
+                }
                 _ => {
                     println!("ERR unexpected character '{}' at position {}", ch, idx);
                     return;
                 }
             }
         }
-
-        println!("EOF");
     }
 
-    fn read_string(&mut self) -> String {
+    fn read_string(&mut self) -> Result<String, TokinzerError> {
         self.next();
 
         let mut result = String::new();
 
-        while let Some((_, ch)) = self.next() {
+        while let Some((idx, ch)) = self.next() {
             if ch == '\\' {
                 match self.next().unwrap().1 {
-                    'n' => {result.push('\n')},
-                    't' => {result.push('\t')},
-                    'r' => {result.push('\r')},
-                    '"' => {result.push('"')},
-                    '\\' => {result.push('\\')},
-                    _ => { continue }
+                    'b' => result.push(char::from(0x08)),
+                    'f' => result.push(char::from(0x0C)),
+                    'n' => result.push('\n'),
+                    't' => result.push('\t'),
+                    'r' => result.push('\r'),
+                    '"' => result.push('"'),
+                    '\\' => result.push('\\'),
+                    '/' => result.push('/'),
+                    'u' => {
+                        let x = self.get_unicode_unit();
+                    }
 
+                    _ => {
+                        return Err(TokinzerError::InvalidToken(ch, idx))
+                    },
                 }
-
             } else if ch == '"' {
                 break;
             } else {
@@ -103,23 +128,17 @@ impl<'a> Tokinzer<'a> {
             }
         }
 
-        result
+        Ok(result)
     }
 
     fn read_number(&mut self) -> String {
-
         let mut number = String::new();
 
         while let Some((_, ch)) = self.peek() {
-            if ch.is_ascii_digit()
-            || ch == '.'
-            || ch == '-'
-            || ch == '+'
-            || ch == 'e'
-            || ch == 'E' {
+            if ch.is_ascii_digit() || ch == '.' || ch == '-' || ch == '+' || ch == 'e' || ch == 'E'
+            {
                 number.push(self.next().unwrap().1);
-            }
-            else {
+            } else {
                 break;
             }
         }
@@ -128,13 +147,12 @@ impl<'a> Tokinzer<'a> {
     }
 
     fn read_value(&mut self) -> String {
-        let mut value  = String::new();
+        let mut value = String::new();
 
         while let Some((_, ch)) = self.peek() {
             if ch.is_ascii_alphabetic() {
                 value.push(self.next().unwrap().1);
-            }
-            else {
+            } else {
                 break;
             }
         }
@@ -142,4 +160,17 @@ impl<'a> Tokinzer<'a> {
         value
     }
 
+    fn get_unicode_unit(&mut self) -> u16 {
+        let mut unicode = self.chars.by_ref().take(4).map(|(_, ch)| ch);
+
+        if unicode.all(|ch| ch.is_ascii_hexdigit()) {
+            return String::from_iter(unicode).parse::<u16>().unwrap();
+        }
+        
+        0
+    }
+}
+
+enum TokinzerError {
+    InvalidToken(char, usize),
 }
